47a62bd684b92f18a59ef4bd46d94b451ab8e7d9
Imagine Engine <58477150+ImagineEngine@users.noreply.github.com>
2021-06-11 14:36:54 +0530

    Auto-Search
    Automatically sets the focus to the input search element

--- src/chromium/newtab.js
+++ src/chromium/newtab.js
@@ -4,6 +4,8 @@ const NEW_TAB_URL = 'http://tab.gladly.io/newtab/'
 try {
   chrome.tabs.getCurrent((tab) => {
     chrome.tabs.update(tab.id, { url: NEW_TAB_URL })
+    // Sets focus to the input search element
+    document.querySelector("#tab-search-id").focus()
   })
 } catch (e) {
   // Fall back to client-side navigation.
